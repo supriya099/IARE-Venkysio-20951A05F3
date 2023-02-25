ecf73fae121174f2117ba12de40cc6a575618cc7
supriya_07 <113609606+supriya099@users.noreply.github.com>
2023-02-25 11:18:26 +0530

    Create program.py

---
+++ program.py
@@ -0,0 +1,9 @@
+x=int(input("Enter the number of toys"))
+y=int(input("Enter the total amount for the toys"))
+z=int(input("Enter the amount left"))
+A=y+z
+B=y//z
+print("The total amount he had")
+print(A)
+print("The cost of one toy")
+print(B)
